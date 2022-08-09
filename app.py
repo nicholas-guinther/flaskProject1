f0c6daaae8f905fff1487c92b2f2683466cc80ab
nicholas-guinther <nickguinther@protonmail.com>
2022-08-09 11:32:57 -0400

    Added General Enpoints with CRUD fucntionality

--- app.py
+++ app.py
@@ -1,12 +1,72 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+stores = [
+    {
+        'name': 'My Store',
+        'items': [
+            {
+                'name': 'Item One',
+                'price': 15.99
+            }
+        ]
+    }
+]
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+# POST /store data: {name}
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+
+# GET /store/<string>:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+
+    return jsonify({'message': 'store not found'})
+
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+
+    return jsonify({'message': 'store not found'})
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
